dc5100a0ab0c82b50cc7762308068f00aad1cd9c
piedra123456 <rosepiedrasingco123@gmail.com>
2021-10-17 12:00:13 +0800

    commit 1

---
+++ waterbilling/waterbill_app/static/js/login/main.js
@@ -0,0 +1,39 @@
+(function() {
+    "use strict";
+
+    var treeviewMenu = $('.app-menu');
+
+    // Toggle Sidebar
+    $('[data-toggle="sidebar"]').click(function(event) {
+        event.preventDefault();
+        $('.app').toggleClass('sidenav-toggled');
+    });
+
+    // Activate sidebar treeview toggle
+    $("[data-toggle='treeview']").click(function(event) {
+        event.preventDefault();
+        if (!$(this).parent().hasClass('is-expanded')) {
+            treeviewMenu.find("[data-toggle='treeview']").parent().removeClass('is-expanded');
+        }
+        $(this).parent().toggleClass('is-expanded');
+    });
+
+    // Set initial active toggle
+    $("[data-toggle='treeview.'].is-expanded").parent().toggleClass('is-expanded');
+
+    //Activate bootstrip tooltips
+    $("[data-toggle='tooltip']").tooltip();
+
+})();
+
+let dateDropdown = document.getElementById('date-dropdown');
+
+let currentYear = new Date().getFullYear();
+let earliestYear = 2015;
+while (currentYear >= earliestYear) {
+    let dateOption = document.createElement('option');
+    dateOption.text = currentYear;
+    dateOption.value = currentYear;
+    dateDropdown.add(dateOption);
+    currentYear -= 1;
+}
---
+++ waterbilling/waterbill_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+def signin(request):
+    return render(request , 'waterbill/signin.html')
+
+def latestl(request):
+    return render(request , 'waterbill/latestl.html')
+
+def latestqv(request):
+    return render(request , 'waterbill/latestqv.html')
+
+def oldl(request):
+    return render(request , 'waterbill/oldl.html')
+
+def oldqv(request):
+    return render(request , 'waterbill/oldqv.html')
+
+def profile(request):
+    return render(request , 'waterbill/profile.html')
