ae9f2a89e2ab40c0040484f23699df4e6997f548
piedra123456 <rosepiedrasingco123@gmail.com>
2021-10-22 16:16:04 +0800

    commit 4

--- waterbilling/waterbill_app/models.py
+++ waterbilling/waterbill_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class account_info(models.Model):
+    accountinfoid = models.CharField(max_length = 45,primary_key = True)
+    firstname = models.CharField(max_length = 45)
+
+    class Meta:
+        db_table = "accountinfo_table"
--- waterbilling/waterbilling/settings.py
+++ waterbilling/waterbilling/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'waterbilling.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'waterbilling',
+        'USER': 'root',
+        'PASSWORD': 'akorani###05',
+        'PORT': 3306,
+        'HOST': '127.0.0.1',
     }
 }
 
